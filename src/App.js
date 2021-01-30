3f2afc25c0597410cf2c784ecc2eb10088be6cec
jeantiston <jean.tiston.work@gmail.com>
2021-01-30 23:28:42 +0800

    Added error handling

--- src/App.js
+++ src/App.js
@@ -5,9 +5,35 @@ import pocketLogo from './assets/desktop/pocket-casts.svg';
 import spotifyLogo from './assets/desktop/spotify.svg';
 import dots from './assets/desktop/bg-pattern-dots.svg';
 
+import React from 'react'
+
 import './App.css';
 
 function App() {
+
+  const [errorMsg, setErrorMsg] = React.useState("")
+
+  const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(e.target[0].value === "") {
+      setErrorMsg("Oops! Please add your email")
+    }
+    else if(!validateEmail(e.target[0].value)){
+      setErrorMsg("Oops! Please check your email")
+    }
+    else{
+      setErrorMsg("")
+    }
+    
+
+  }
+
   return (
     <div className="App">
       <div className="wrapper" >
@@ -21,10 +47,11 @@ function App() {
               Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, 
               Apple Podcasts, Google Podcasts, Pocket Casts and more!
             </p>
-            <form>
-              <input type="email" name="email" placeholder="Email address" />
-              <input type="submit" value="Request Access" />
+            <form onSubmit={handleSubmit}>
+              <input type="text" name="email" placeholder="Email address" />
+              <input type="submit" value="Request Access"   />
             </form>
+            { errorMsg && <p className="error">{errorMsg}</p> }
             <div className="logos">
               <div><img src={spotifyLogo} className="logo" alt="Spotify Logo" /></div>
               <div><img src={appleLogo} className="logo" alt="Apple Podcast" /></div>
