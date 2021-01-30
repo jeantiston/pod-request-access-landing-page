023c5d5301cf7561c3d0471831633789be1e78fc
jeantiston <jean.tiston.work@gmail.com>
2021-01-30 17:37:51 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import logo from './assets/desktop/logo.svg';
 import './App.css';
+
+const main = {
+  width: "100vw",
+  height: "100vh"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={main}>
+        <h1>hello</h1>
+      </div>
     </div>
   );
 }
